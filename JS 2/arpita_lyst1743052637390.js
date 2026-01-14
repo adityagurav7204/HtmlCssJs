5e89a20005a3a999c2dcac1e3ecb80da7169472f
adityagurav7204 <adityamg0712@gmail.com>
2026-01-14 13:56:23 +0530

    Add files via upload

---
+++ JS 2/arpita_lyst1743052637390.js
@@ -0,0 +1,4 @@
+document.getElementById("arpita").innerHTML = "This is Arpita!";
+document.getElementById("arpita").style.fontFamily = "Courier";
+document.getElementById("arpita").style.color = "Blue";
+document.getElementById("arpita").style.fontSize = "50px";
